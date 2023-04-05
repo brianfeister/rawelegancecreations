28243f0bb542065f5f67e6ef974fac42d34e78b7
Brian Feister <bfeister@salesforce.com>
2023-04-05 08:38:08 -0600

    use module.exports instead of es6 export statement for older node version

--- functions/utils.js
+++ functions/utils.js
@@ -1,17 +1,18 @@
-export const logAndReturnError = (message, err, statusCode) => {
-  console.log(
-    `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} ${message}: ${err}`
-  );
-  return {
-    statusCode: statusCode || 500,
-    body: JSON.stringify({
-      error: err,
-    }),
-  };
-};
-
-export const logError = (message, err) => {
-  console.log(
-    `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} ${message}: ${err}`
-  );
+module.exports = {
+  logAndReturnError: (message, err, statusCode) => {
+    console.log(
+      `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} ${message}: ${err}`
+    );
+    return {
+      statusCode: statusCode || 500,
+      body: JSON.stringify({
+        error: err,
+      }),
+    };
+  },
+  logError: (message, err) => {
+    console.log(
+      `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} ${message}: ${err}`
+    );
+  },
 };
